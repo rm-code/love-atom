e531f8775f52d229ca518fe7788bf4473fe61f12
Robert Machmer <robert.machmer@gmail.com>
2017-04-26 23:50:36 +0200

    Rewrite JSON generator
    The JSON generator has been changed to create a template file which
    follows the formatting used by atom-autocomplete-lua.

--- generator/json-generator.lua
+++ generator/json-generator.lua
@@ -1,135 +1,212 @@
+local json = require 'dkjson'
+
 -- ------------------------------------------------
 -- Constants
 -- ------------------------------------------------
 
-local APOSTROPHE =  '"'
-local COMMA      =  ','
-local LINE_BREAK = '\n'
-local TAB        = '  '
-
-local PARAM  = '${%d:%s (%s)}'
-
 local OUTPUT_FILE = 'love-completions.json'
-
 local WIKI_URL = 'https://love2d.org/wiki/'
+
+local LOVE_MODULE_STRING = 'love.'
+
+-- JSON Output control
+local DEBUG = false
+local KEY_ORDER = {
+    'type',
+    'description',
+    'fields',
+    'args',
+    'returnTypes',
+    'link'
+}
 
 -- ------------------------------------------------
 -- Local Functions
 -- ------------------------------------------------
 
-local function generateArguments( arguments, start )
-    local params = ''
-    start = start or 0
+---
+-- Generates a sequence containing subtables of all the arguments
+--
+local function createArguments( args )
+    local arguments = {}
+    for i, v in ipairs( args ) do
+        arguments[i] = {}
+        arguments[i].name = v.name
 
-    if arguments then
-        for i, args in ipairs( arguments ) do
-            params = params .. string.format( PARAM, start + i, args.name, args.type )
+        -- Use [foo] notation as display name for optional arguments.
+        if v.default then
+            arguments[i].displayName = string.format( '[%s]', v.name )
+        end
+    end
+    return arguments
+end
 
-            -- Add a separator unless we are dealing with the last argument of the function.
-            if i ~= #arguments then
-                params = params .. ', '
+local function getType( type )
+    if type == 'string'
+    or type == 'number'
+    or type == 'table'
+    or type == 'boolean'
+    or type == 'function' then
+        return { type = type }
+    end
+    return { type = 'ref', name = type }
+end
+
+local function createReturnTypes( returns )
+    local returnTypes = {}
+    for i, v in ipairs( returns ) do
+        returnTypes[i] = getType( v.type )
+    end
+    return returnTypes
+end
+
+-- TODO handle functions with neither args nor return variables.
+local function createVariant( vdef )
+    local variant = {}
+    if vdef.arguments then
+        variant.args = createArguments( vdef.arguments )
+    end
+
+    if vdef.returns then
+        variant.returnTypes = createReturnTypes( vdef.returns )
+    end
+    return variant
+end
+
+local function createFunction( f, parent, moduleString )
+    local name = f.name
+    parent[name] = {}
+    parent[name].type = 'function'
+
+    parent[name].description = f.description
+    parent[name].link = string.format( "%s%s%s", WIKI_URL, moduleString, name )
+
+    -- Create normal function if there is just one variant.
+    if #f.variants == 1 then
+        local v = createVariant( f.variants[1] )
+        parent[name].args = v.args
+        parent[name].returnTypes = v.returnTypes
+        return
+    end
+
+    -- Generate multiple variants.
+    parent[name].variants = {}
+    for i, v in ipairs( f.variants ) do
+        parent[name].variants[i] = createVariant( v )
+    end
+end
+
+local function createLoveFunctions( functions, parent )
+    for _, f in pairs( functions ) do
+        createFunction( f, parent, LOVE_MODULE_STRING )
+    end
+end
+
+local function createLoveModuleFunctions( modules, parent )
+    for _, m in pairs( modules ) do
+        local name = m.name
+        parent[name] = {}
+        parent[name].type = 'table'
+
+        parent[name].description = m.description
+        parent[name].link = string.format( "%s%s%s", WIKI_URL, LOVE_MODULE_STRING, name )
+
+        parent[name].fields = {}
+
+        for _, f in pairs( m.functions ) do
+            createFunction( f, parent[name].fields, string.format( '%s%s.', LOVE_MODULE_STRING, name ))
+        end
+    end
+end
+
+local function createGlobals( api, global )
+    global.type = 'table'
+    global.fields = {}
+    global.fields.love = {}
+    global.fields.love.type = 'table'
+    global.fields.love.fields = {}
+
+    createLoveFunctions( api.functions, global.fields.love.fields )
+    createLoveFunctions( api.callbacks, global.fields.love.fields )
+    createLoveModuleFunctions( api.modules, global.fields.love.fields )
+end
+
+-- ------------------------------------------------
+-- Creation methods for named types
+-- ------------------------------------------------
+
+
+local function collectTypes( api )
+    local types = {}
+
+    for _, type in pairs( api.types ) do
+        assert( not types[type.name] )
+        types[type.name] = type
+    end
+
+    for _, module in pairs( api.modules ) do
+        if module.types then -- Not all modules define types.
+            for _, type in pairs( module.types ) do
+                assert( not types[type.name] )
+                types[type.name] = type
             end
         end
     end
 
-    return params
+    return types
 end
 
-local function cleanUpString( str )
-    return str:gsub( '\n\n', ' ' ):gsub( '\"', '\\"' ):gsub( '\0', '' )
-end
+local function createTypes( types, namedTypes )
+    for name, type in pairs( types ) do
+        print( '  ' .. name )
+        namedTypes[name] = {}
+        namedTypes[name].type = 'table'
+        namedTypes[name].fields = {}
 
-local function buildCallbackCompletion( f )
-    local arguments = generateArguments( f.variants[1].arguments )
-    return {
-        TAB .. '{' .. LINE_BREAK,
-        TAB .. TAB .. '"displayText": "love.' .. f.name .. APOSTROPHE .. COMMA .. LINE_BREAK,
-        TAB .. TAB .. '"type": "function"' .. COMMA .. LINE_BREAK,
-        TAB .. TAB .. '"description": ' .. APOSTROPHE .. cleanUpString( f.description ) .. APOSTROPHE .. COMMA .. LINE_BREAK,
-        TAB .. TAB .. '"descriptionMoreURL": ' .. APOSTROPHE .. WIKI_URL .. 'love.' .. f.name .. APOSTROPHE .. COMMA .. LINE_BREAK,
-        TAB .. TAB .. '"snippet": ',
-        APOSTROPHE .. 'love.' .. string.format( f.name .. '(%s)', arguments ) .. APOSTROPHE .. LINE_BREAK,
-        TAB .. '},' .. LINE_BREAK
-    }
-end
+        if type.functions then
+            for _, f in pairs( type.functions ) do
+                createFunction( f, namedTypes[name].fields, name .. ':' )
+            end
+        end
 
-local function buildModuleFunctionCompletion( f, module, closing )
-    local arguments = generateArguments( f.variants[1].arguments )
-    return {
-        TAB .. '{' .. LINE_BREAK,
-        TAB .. TAB .. '"displayText": "love.' .. module.name .. '.' .. f.name .. APOSTROPHE .. COMMA .. LINE_BREAK,
-        TAB .. TAB .. '"type": "function"' .. COMMA .. LINE_BREAK,
-        TAB .. TAB .. '"description": ' .. APOSTROPHE .. cleanUpString( f.description ) .. APOSTROPHE .. COMMA .. LINE_BREAK,
-        TAB .. TAB .. '"descriptionMoreURL": ' .. APOSTROPHE .. WIKI_URL .. string.format( 'love.%s.%s', module.name, f.name ) .. APOSTROPHE .. COMMA .. LINE_BREAK,
-        TAB .. TAB .. '"snippet": ',
-        APOSTROPHE .. string.format( 'love.%s.%s(%s)', module.name, f.name, arguments ) .. APOSTROPHE .. LINE_BREAK,
-        TAB .. (  closing and '}' or '},'  ) .. LINE_BREAK
-    }
-end
-
-local function buildTypeFunctionCompletion( f, type, closing )
-    local arguments = generateArguments( f.variants[1].arguments, 2 )
-    return {
-        TAB .. '{' .. LINE_BREAK,
-        TAB .. TAB .. '"displayText": "' .. type.name .. ':' .. f.name .. APOSTROPHE .. COMMA .. LINE_BREAK,
-        TAB .. TAB .. '"type": "function"' .. COMMA .. LINE_BREAK,
-        TAB .. TAB .. '"description": ' .. APOSTROPHE .. cleanUpString( f.description ) .. APOSTROPHE .. COMMA .. LINE_BREAK,
-        TAB .. TAB .. '"descriptionMoreURL": ' .. APOSTROPHE .. WIKI_URL .. string.format( '%s:%s', type.name, f.name ) .. APOSTROPHE .. COMMA .. LINE_BREAK,
-        TAB .. TAB .. '"snippet": ',
-        APOSTROPHE .. string.format( '${%d:%s}:%s(%s)', 1, type.name, f.name, arguments ) .. APOSTROPHE .. LINE_BREAK,
-        TAB .. (  closing and '}' or '},'  ) .. LINE_BREAK
-    }
+        if type.supertypes then
+            namedTypes[name].metatable = {}
+            namedTypes[name].metatable.type = 'table'
+            namedTypes[name].metatable.fields = {
+                __index = {
+                    type = 'ref',
+                    name = type.parenttype
+                }
+            }
+        end
+    end
 end
 
 local function createJSON()
     print( 'Generating LOVE snippets ... ' )
 
-    local file = io.open( OUTPUT_FILE, 'w' )
-    assert( file, "ERROR: Can't write file: " .. OUTPUT_FILE )
-
     -- Load the LÖVE api files.
     local api = require( 'api.love_api' )
+    assert( api, 'No api file found!' )
+    print( 'Found API file for LÖVE version ' .. api.version )
 
-    -- Create file header.
-    file:write( '[' .. LINE_BREAK )
+    -- Create main table.
+    local output = {
+        global = {},
+        namedTypes = {},
+    }
 
-    -- Create completions for LÖVE callbacks.
-    print( TAB .. 'Writing callbacks' )
-    for _, f in ipairs( api.callbacks ) do
-        local str = buildCallbackCompletion( f )
-        for i = 1, #str do
-            file:write( str[i] )
-        end
-    end
+    print( 'Generating functions ...' )
+    createGlobals( api, output.global )
 
-    -- Generate the snippets for all LÖVE modules.
-    print( TAB .. 'Writing modules' )
-    for i, module in ipairs( api.modules ) do
-        print( TAB .. TAB .. '- ' .. module.name )
+    print( 'Generating types ...' )
+    local types = collectTypes( api )
+    createTypes( types, output.namedTypes )
 
-        if module.types then
-            for _, type in ipairs( module.types ) do
-                print( TAB .. TAB .. TAB .. '-> ' .. type.name )
-
-                for _, f in ipairs( type.functions ) do
-                    local str = buildTypeFunctionCompletion( f, type )
-                    for l = 1, #str do
-                        file:write( str[l] )
-                    end
-                end
-            end
-        end
-
-        for j, f in ipairs( module.functions ) do
-            local str = buildModuleFunctionCompletion( f, module, ( i == #api.modules and j == #module.functions )  )
-            for k = 1, #str do
-                file:write( str[k] )
-            end
-        end
-    end
-
-    file:write( ']' )
-    file:close()
+    local jsonString = json.encode( output, { indent = DEBUG, keyorder = KEY_ORDER })
+    local file = io.open( OUTPUT_FILE, 'w' )
+    assert( file, "ERROR: Can't write file: " .. OUTPUT_FILE )
+    file:write( jsonString )
 
     print( 'DONE!' )
 end
